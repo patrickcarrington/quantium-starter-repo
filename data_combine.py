415ddea79485a8ac644cdf0a88b9ff09259c2911
Patrick Carrington <59370480+patrickcarrington@users.noreply.github.com>
2022-05-23 13:37:57 +0100

    commenting

--- data_combine.py
+++ data_combine.py
@@ -1,20 +1,20 @@
 import csv
-with open('data/daily_sales_data_0.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'w') as out:
+with open('data/daily_sales_data_0.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'w') as out: #'w' to create daily_sales_data_comb
     writer = csv.writer(out)
     for row in csv.reader(inp):
-        if row[0] == "pink morsel":
-            writer.writerow(row)
+        if row[0] == "pink morsel": #only find pink morsel sales
+            writer.writerow(row) #write row if a pink morsel sale
 
-with open('data/daily_sales_data_1.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'a') as out:
+with open('data/daily_sales_data_1.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'a') as out: #'a' to append to daily_sales_data_comb
     writer = csv.writer(out)
     for row in csv.reader(inp):
-        if row[0] == "pink morsel":
-            writer.writerow(row)
+        if row[0] == "pink morsel": #only find pink morsel sales
+            writer.writerow(row) #write row if a pink morsel sale
 
-with open('data/daily_sales_data_2.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'a') as out:
+with open('data/daily_sales_data_2.csv', 'r') as inp, open('data/daily_sales_data_comb.csv', 'a') as out: #'a' to append to daily_sales_data_comb
     writer = csv.writer(out)
     for row in csv.reader(inp):
-        if row[0] == "pink morsel":
-            writer.writerow(row)
+        if row[0] == "pink morsel":#only find pink morsel sales
+            writer.writerow(row) #write row if a pink morsel sale
 
 
--- data_sales.py
+++ data_sales.py
@@ -4,6 +4,6 @@ with open('data/daily_sales_data_comb.csv', 'r') as inp, open('data/output.csv',
     writer = csv.writer(out)
 
     for row in csv.reader(inp):
-        row[1] = row[1].replace('$','')
-        sales = float(row[1]) * int(row[2])
-        writer.writerow((sales, row[3], row[4]))
+        row[1] = row[1].replace('$','') #remove dollar sign
+        sales = float(row[1]) * int(row[2]) #multiply sales and quantity
+        writer.writerow((sales, row[3], row[4])) #wrire
